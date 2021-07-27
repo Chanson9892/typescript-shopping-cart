1be2eb81f1e636f8f328118d09f8ee5e7d575c46
Chanson9892 <chan.hanson5@gmail.com>
2021-07-27 16:44:34 -0700

    added imports to app folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-function App() {
+//components
+import { Drawer } from '@material-ui/core';
+import { LinearProgress } from '@material-ui/core';
+import Grid  from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+//style 
+import { Wrapper } from './App.styles';
+
+//api
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      start
     </div>
   );
 }
