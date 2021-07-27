e6a71c5c4e48789889a69ac3d65a0173ca4eb031
Chanson9892 <chan.hanson5@gmail.com>
2021-07-27 16:57:01 -0700

    made our fetch arrow function and made our type for the cart item

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,21 @@ import Badge from '@material-ui/core/Badge';
 //style 
 import { Wrapper } from './App.styles';
 
+//types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
 //api
+const getProducts = async (): Promise<CartItemType> =>  // using promise because of the await
+  await (await fetch('https://fakestoreapi.com/products')).json()  // the inner await is for the api call. outer is for converting it to json.
+
 
 const App = () => {
   return (
